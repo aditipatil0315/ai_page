c03ac99c370eb01e625c34e8d5ce5fe28333aeb4
Aditi Patil <aditiprakashpatil7@gmail.com>
2025-03-20 23:50:13 +0530

    edited

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -5,8 +5,9 @@ const Section2 = () => {
   return (
     <div className="section2-wrapper">
       <div className="scale_section">
-        <h1>Scale Smarter with our Site of Artificial Intelligence Services and Solutions.</h1>
+        <h1><span>Scale Smarter</span> with our Site of Artificial Intelligence Services and Solutions.</h1>
         <h3>Explore our comprehensive suite of AI services designed to drive profitable innovations and deliver tangible outcomes. From consulting to development and beyond, our extensive artificial intelligence services and solutions are tailored to meet unique business needs in any industry.</h3>
+
         <br />
         <div className="applications_card">
           <h2>Business Applications</h2>
@@ -31,12 +32,12 @@ const Section2 = () => {
               Ethical and Responsible AI Framework Integration
             </div>
           </div>
-
           <br />
           <br />
 
-          <h2>Our Offerings</h2>
-          <p>Unlock the full creative potential of gen AI with our Generative AI solutions that help your team craft tailored roadmaps seamlessly integrating impactful use cases that drive innovation and efficiency.</p>
+        
+          <h2><span>Our Offerings</span></h2>
+          <h3>Unlock the full creative potential of gen AI with our Generative AI solutions that help your team craft tailored roadmaps seamlessly integrating impactful use cases that drive innovation and efficiency.</h3>
         </div>
       </div>
     </div>
